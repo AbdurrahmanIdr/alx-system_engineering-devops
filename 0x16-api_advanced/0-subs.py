d89c7e002fb6e24bf80e1c716a2b3c447f6e28be
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-06-08 12:30:42 +0100

    0-subs.py number_of_subscribers statuscode to 400

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
         data = response.json()
