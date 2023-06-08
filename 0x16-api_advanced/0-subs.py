4c46d3e07f136a832863f13afcfefccbab28d944
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-06-08 12:40:14 +0100

    0-subs.py number_of_subscribers returns 0 for false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,12 +16,8 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
         return 0
-
+    results = response.json().get("data")
+    return results.get("subscribers")
