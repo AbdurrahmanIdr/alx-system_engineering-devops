ed8210da9f5d4edbc581e6c17413ea194b741c39
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-06-08 13:38:19 +0100

    PEP8 Validation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,22 +2,28 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API to get the number of subscribers for a given subreddit.
+    Queries the Reddit API to get the number of subscribers
+    for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
-        int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid.
+        int: The number of subscribers of the subreddit.
+        Returns 0 if the subreddit is invalid.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 404:
         return 0
+
     results = response.json().get("data")
     return results.get("subscribers")
