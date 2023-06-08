8320eb6932c3abee5169f1d8297ac95b41a54de5
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-06-08 12:20:28 +0100

    0-subs.py Implement number_of_subscribers function

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API to get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
+
